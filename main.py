6584f51e7195b579ed7b4981732c2a608f1193e1
Varshit Dusad <dusadv@BBRs-MacBook-Pro.local>
2023-12-30 07:47:34 +0530

    Added the list of algorithms to cover on main page

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 import streamlit as st
 
-st.title("Basic Deep Learning")
+st.title("Machine Learning from Scratch")
 
-st.subheader("Simple Neural Network")
+st.subheader("Following are the algorithms that will be covered")
+
+st.divider()
+
+st.markdown("1. K Nearest Neighbor")
+st.markdown("2. Linear Regression")
+st.markdown("3. Logistic Regression")
+st.markdown("4. Decision Trees")
+st.markdown("5. Random Forest")
+st.markdown("6. Naive Bayes")
+st.markdown("7. Principal Component Analysis")
+st.markdown("8. Perceptron")
+st.markdown("9. Support Vector Machine")
+st.markdown("10. K-Means Clustering")
 
