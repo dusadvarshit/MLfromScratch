a6352b8d7d87eaceabb5f5635f238e3a2d6ecc3d
Varshit Dusad <dusadv@BBRs-MacBook-Pro.local>
2024-01-01 08:27:12 +0530

    Added Support Vector Machine

--- pages/8_SupportVectorMachine.py
+++ pages/8_SupportVectorMachine.py
@@ -1,26 +1,108 @@
 import streamlit as st
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn import datasets
+import matplotlib.pyplot as plt
 
-st.title("Linear Regression")
+st.title("Support Vector Machine")
 
 st.subheader('Theory')
 on = st.toggle(">")
 
 ## Writing Theory 
 if on:
-    st.write("""Linear Regression is a Machine Learning based modeling technique where 
-             a independent variable is modelled as a weighted sum of one or more dependent variable.
+    st.write("""Support vector machine is a non-linear modelling technique primarily used
+     to classify (but also can be used for regression) dependent variables.
              """)
     
-    st.write("""The model is considered to be linear. This means for a specific change 
-             in one unit of $$X$$, we can determine it's impact on $$y$$.""")
+    st.write("""It belongs to the class of models that applies Kernel techniques.""")
     
-    st.write("""Mathematically, the linear regression is represented by:""")
+    st.write("""Kernel techniques is a mathematical trick to represent input vectors
+    in higher dimensions without doing the complete calculation.""")
     
-    st.latex(r'''
-    f(x) = \beta_{0} + \beta_{0}X_{1} + \beta_{0}X_{2} .... + \beta_{0}X_{p}
-    ''')
-
-    st.write("""Do note that Linear Regression is almost always an approximation 
-             of the real world.""")
 
 st.divider()
+
+class SVM:
+
+    def __init__(self, learning_rate=0.001, lambda_param=0.01, n_iters=1000):
+        self.lr = learning_rate
+        self.lambda_param = lambda_param
+        self.n_iters = n_iters
+        self.w = None
+        self.b = None
+
+    def fit(self, X, y):
+        n_samples, n_features = X.shape
+
+        y_ = np.where(y <= 0, -1, 1)
+
+        # init weights
+        self.w = np.zeros(n_features)
+        self.b = 0
+
+        for _ in range(self.n_iters):
+            for idx, x_i in enumerate(X):
+                condition = y_[idx] * (np.dot(x_i, self.w) - self.b) >= 1
+                if condition:
+                    self.w -= self.lr * (2 * self.lambda_param * self.w)
+                else:
+                    self.w -= self.lr * (2 * self.lambda_param * self.w - np.dot(x_i, y_[idx]))
+                    self.b -= self.lr * y_[idx]
+
+
+    def predict(self, X):
+        approx = np.dot(X, self.w) - self.b
+        return np.sign(approx)
+
+def accuracy(y_true, y_pred):
+    accuracy = np.sum(y_true == y_pred) / len(y_true)
+    return accuracy
+
+X, y = datasets.make_blobs(
+    n_samples=50, n_features=2, centers=2, cluster_std=1.05, random_state=40
+)
+y = np.where(y == 0, -1, 1)
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=123
+)
+
+clf = SVM()
+clf.fit(X_train, y_train)
+predictions = clf.predict(X_test)
+
+
+def visualize_svm():
+    def get_hyperplane_value(x, w, b, offset):
+        return (-w[0] * x + b + offset) / w[1]
+
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+    plt.scatter(X[:, 0], X[:, 1], marker="o", c=y)
+
+    x0_1 = np.amin(X[:, 0])
+    x0_2 = np.amax(X[:, 0])
+
+    x1_1 = get_hyperplane_value(x0_1, clf.w, clf.b, 0)
+    x1_2 = get_hyperplane_value(x0_2, clf.w, clf.b, 0)
+
+    x1_1_m = get_hyperplane_value(x0_1, clf.w, clf.b, -1)
+    x1_2_m = get_hyperplane_value(x0_2, clf.w, clf.b, -1)
+
+    x1_1_p = get_hyperplane_value(x0_1, clf.w, clf.b, 1)
+    x1_2_p = get_hyperplane_value(x0_2, clf.w, clf.b, 1)
+
+    ax.plot([x0_1, x0_2], [x1_1, x1_2], "y--")
+    ax.plot([x0_1, x0_2], [x1_1_m, x1_2_m], "k")
+    ax.plot([x0_1, x0_2], [x1_1_p, x1_2_p], "k")
+
+    x1_min = np.amin(X[:, 1])
+    x1_max = np.amax(X[:, 1])
+    ax.set_ylim([x1_min - 3, x1_max + 3])
+
+    st.pyplot(fig)
+
+visualize_svm()
+
+st.write("SVM classification accuracy", accuracy(y_test, predictions))
