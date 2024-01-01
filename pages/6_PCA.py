2005b11bf8c99440fd96addcd21f674f1a12c1a0
Varshit Dusad <dusadv@BBRs-MacBook-Pro.local>
2024-01-02 05:06:36 +0530

    Added the code for PCA

--- pages/6_PCA.py
+++ pages/6_PCA.py
@@ -1,26 +1,84 @@
 import streamlit as st
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn import datasets
 
-st.title("Linear Regression")
+st.title("Principal Component Analysis")
 
 st.subheader('Theory')
 on = st.toggle(">")
 
 ## Writing Theory 
 if on:
-    st.write("""Linear Regression is a Machine Learning based modeling technique where 
-             a independent variable is modelled as a weighted sum of one or more dependent variable.
+    st.write("""Principal Component Analysis is a dimensionality reduction.
+            It is common in ML world to work with very large number of features
+            to model predictions.
+            But, high dimensions can lead to some complex challenges.  
              """)
     
-    st.write("""The model is considered to be linear. This means for a specific change 
-             in one unit of $$X$$, we can determine it's impact on $$y$$.""")
+    st.write("""PCA helps in 2 ways:""")
     
-    st.write("""Mathematically, the linear regression is represented by:""")
-    
-    st.latex(r'''
-    f(x) = \beta_{0} + \beta_{0}X_{1} + \beta_{0}X_{2} .... + \beta_{0}X_{p}
-    ''')
+    st.write("""1. It allows us to graphically visualise our feature space
+    and hence providing useful insights.""")
 
-    st.write("""Do note that Linear Regression is almost always an approximation 
-             of the real world.""")
+    st.write("""2. It reduces the length of feature vector in our model. 
+            Making it accessible for traditional ML techniques.""")
+
 
 st.divider()
+
+class PCA:
+
+    def __init__(self, n_components):
+        self.n_components = n_components
+        self.components = None
+        self.mean = None
+
+    def fit(self, X):
+        # mean centering
+        self.mean = np.mean(X, axis=0)
+        X = X -  self.mean
+
+        # covariance, functions needs samples as columns
+        cov = np.cov(X.T)
+
+        # eigenvectors, eigenvalues
+        eigenvectors, eigenvalues = np.linalg.eig(cov)
+
+        # eigenvectors v = [:, i] column vector, transpose this for easier calculations
+        eigenvectors = eigenvectors.T
+
+        # sort eigenvectors
+        idxs = np.argsort(eigenvalues)[::-1]
+        eigenvalues = eigenvalues[idxs]
+        eigenvectors = eigenvectors[idxs]
+
+        self.components = eigenvectors[:self.n_components]
+
+    def transform(self, X):
+        # projects data
+        X = X - self.mean
+        return np.dot(X, self.components.T)
+
+data = datasets.load_iris()
+X = data.data
+y = data.target
+
+pca = PCA(2)
+pca.fit(X)
+X_projected = pca.transform(X)
+
+x1 = X_projected[:, 0]
+x2 = X_projected[:, 1]
+
+fig, ax = plt.subplots(figsize=(6, 4))
+im = ax.scatter(
+    x1, x2, c=y, edgecolor="none", alpha=0.8, cmap=plt.cm.get_cmap("viridis", 3)
+)
+
+ax.set_xlabel("Principal Component 1")
+ax.set_ylabel("Principal Component 2")
+fig.colorbar(im, ax=ax)
+
+st.pyplot(fig)
+
